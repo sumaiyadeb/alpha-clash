dbeae3d9042ff22b497642c998f068fc1c76cd4f
sumaiyadeb <programing.hero1@hmail.com>
2024-02-14 15:36:31 -0600

    last commit

--- script/alpha.js
+++ script/alpha.js
@@ -1,3 +1,30 @@
 function play() {
-  console.log("play start now");
+  // step: hide the home screen. to hide the screen add the class hidden to the home section
+  //   const homeSection = document.getElementById("home-screen");
+  //   homeSection.classList.add("hidden");
+  //   console.log(homeSection);
+  // show the playground
+  //   const playgroundSection = document.getElementById("play-ground");
+  //   playgroundSection.classList.remove("hidden");
+  //   console.log(playgroundSection.classList);
+}
+// function hideElementById(elementId) {
+//   const element = document.getElementById(elementId);
+// }
+function continueGame() {
+  // step-1: generate a random  alphabet
+  const alphabet = getARandomAlphabet();
+  console.log("your random alphabet", alphabet);
+
+  // set set randomly generated alphabet to the screen  (show it)
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  currentAlphabetElement.innerText = alphabet;
+  // set background color
+  setBackgroundColorById(alphabet);
+}
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
 }
---
+++ script/utility.js
@@ -0,0 +1,36 @@
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
+}
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
+}
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-800");
+}
+
+// function getARandomAlphabet() {
+// get or create an alphabet array
+//   const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+//   const alphabets = alphabetString.split("");
+//   console.log(alphabets);
+// get a random index between 0-25
+//   const randomNumber = Math.random() * 25;
+//   const index = Math.round(randomNumber);
+
+//   const alphabet = alphabets[index];
+//   console.log(index, alphabet);
+//   return alphabet;
+// }
+function getARandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabetString.split("");
+
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+
+  const alphabet = alphabets[index];
+  return alphabet;
+}
